0cb1854fdf14d86ad151422c89fbd35eae791e3e
Max Schwartz <maxwell.schwartz11@gmail.com>
2016-12-13 21:02:10 -0500

    Cleaned up with functions and comments

--- sms_analyzer.py
+++ sms_analyzer.py
@@ -2,33 +2,57 @@
 import sqlite3
 import os
 
-parent_dir = input('Enter path to directory >> ')
-os.chdir(parent_dir)
+def locate_sms_db():
+    '''Find the name and location of the sms database'''
 
-conn1 = sqlite3.connect('Manifest.db')
-c1 = conn1.cursor()
-sms_location = []
-for row in c1.execute("SELECT fileID FROM Files WHERE relativePath LIKE '%sms.db';"):
-    sms_location.append(row)
+    parent_dir = input('Enter path to directory >> ')
+    os.chdir(parent_dir)
 
-sms_db = sms_location[0][0]
-sms_dir = sms_db[:2]
-os.chdir(sms_dir)
+    conn1 = sqlite3.connect('Manifest.db')
+    c1 = conn1.cursor()
+    sms_location = []
+    # only one item should be found, but just in case
+    for row in c1.execute("SELECT fileID FROM Files WHERE relativePath LIKE '%sms.db';"):
+        sms_location.append(row)
 
-conn2 = sqlite3.connect(sms_db)
-c2 = conn2.cursor()
+    # database name is first item in list
+    sms_db = sms_location[0][0]
+    # directory name is first two letters/numbers in database name
+    sms_dir = sms_db[:2]
 
-freq = {}
-for row in c2.execute("SELECT handle.id, message.text FROM message JOIN handle ON message.handle_id = handle.ROWID WHERE message.is_from_me = 0;"):
-    if row[0] in freq:
-        freq[row[0]] += 1
-    else:
-        freq[row[0]] = 1
+    return sms_dir, sms_db
 
-freq_tup = list(freq.items())
-for i in range(len(freq_tup)):
-    freq_tup[i] = list(freq_tup[i])
-    freq_tup[i].reverse()
-freq_tup.sort()
+def get_sms_freq(c):
+    '''Calculate the frequency of messages received from each number'''
 
-print(freq_tup[-1][1] + ' texted you the most often. You received ' + str(freq_tup[-1][0]) + ' messages from them!')
+    # dictionary of each number that has sent messages and their counts
+    freq = {}
+    for row in c.execute("SELECT handle.id, message.text FROM message JOIN handle ON message.handle_id = handle.ROWID WHERE message.is_from_me = 0;"):
+        if row[0] in freq:
+            freq[row[0]] += 1
+        else:
+            freq[row[0]] = 1
+
+    # convert to list of phone numbers and count; sort by frequency
+    freq_tup = list(freq.items())
+    for i in range(len(freq_tup)):
+        freq_tup[i] = list(freq_tup[i])
+        freq_tup[i].reverse()
+    freq_tup.sort()
+
+    return freq_tup
+
+def main():
+
+    sms_dir, sms_db = locate_sms_db()
+    os.chdir(sms_dir)
+
+    conn2 = sqlite3.connect(sms_db)
+    c2 = conn2.cursor()
+
+    sms_freq = get_sms_freq(c2)
+
+
+    print(sms_freq[-1][1] + ' texted you the most often. You received ' + str(sms_freq[-1][0]) + ' messages from them!')
+
+main()
